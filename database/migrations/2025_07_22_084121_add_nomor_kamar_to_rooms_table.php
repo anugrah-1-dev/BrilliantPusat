a50166c5779b727840f71db2daf12886b60f03fe
Epangssss <edwardlpramudya2005@gmail.com>
2025-07-22 22:04:45 +0700

    part9

---
+++ database/migrations/2025_07_22_084121_add_nomor_kamar_to_rooms_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    // migration file
+    public function up()
+    {
+        Schema::table('rooms', function (Blueprint $table) {
+            $table->string('nomor_kamar')->after('nama')->unique();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('rooms', function (Blueprint $table) {
+            //
+        });
+    }
+};
