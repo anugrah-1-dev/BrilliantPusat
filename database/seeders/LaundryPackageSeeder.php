d365e9fd2586bfcc6b35a80338e7c2ef9b6f4065
anugrah-1-dev <tejomaliki1@gmail.com>
2026-05-19 18:49:32 +0700

    fix: correct periode type (int) and status enum in LaundryPackageSeeder

--- database/seeders/LaundryPackageSeeder.php
+++ database/seeders/LaundryPackageSeeder.php
@@ -14,8 +14,8 @@ class LaundryPackageSeeder extends Seeder
                 'nama_paket' => '1 Minggu',
                 'harga'      => 56000,
                 'jenis'      => 'mingguan',
-                'periode'    => '1 Minggu',
-                'status'     => 'active',
+                'periode'    => 1,
+                'status'     => 'aktif',
                 'deskripsi'  => 'Max 7 kg',
                 'thumbnail'  => null,
             ],
@@ -23,8 +23,8 @@ class LaundryPackageSeeder extends Seeder
                 'nama_paket' => '2 Minggu',
                 'harga'      => 112000,
                 'jenis'      => 'mingguan',
-                'periode'    => '2 Minggu',
-                'status'     => 'active',
+                'periode'    => 2,
+                'status'     => 'aktif',
                 'deskripsi'  => 'Max 14 kg',
                 'thumbnail'  => null,
             ],
@@ -32,8 +32,8 @@ class LaundryPackageSeeder extends Seeder
                 'nama_paket' => '1 Bulan',
                 'harga'      => 240000,
                 'jenis'      => 'bulanan',
-                'periode'    => '1 Bulan',
-                'status'     => 'active',
+                'periode'    => 1,
+                'status'     => 'aktif',
                 'deskripsi'  => 'Max 30 kg',
                 'thumbnail'  => null,
             ],
@@ -41,8 +41,8 @@ class LaundryPackageSeeder extends Seeder
                 'nama_paket' => '2 Bulan',
                 'harga'      => 480000,
                 'jenis'      => 'bulanan',
-                'periode'    => '2 Bulan',
-                'status'     => 'active',
+                'periode'    => 2,
+                'status'     => 'aktif',
                 'deskripsi'  => 'Max 60 kg',
                 'thumbnail'  => null,
             ],
@@ -50,8 +50,8 @@ class LaundryPackageSeeder extends Seeder
                 'nama_paket' => '3 Bulan',
                 'harga'      => 720000,
                 'jenis'      => 'bulanan',
-                'periode'    => '3 Bulan',
-                'status'     => 'active',
+                'periode'    => 3,
+                'status'     => 'aktif',
                 'deskripsi'  => 'Max 90 kg',
                 'thumbnail'  => null,
             ],
@@ -59,7 +59,7 @@ class LaundryPackageSeeder extends Seeder
 
         foreach ($packages as $package) {
             LaundryPackage::firstOrCreate(
-                ['nama_paket' => $package['nama_paket'], 'periode' => $package['periode']],
+                ['nama_paket' => $package['nama_paket'], 'jenis' => $package['jenis']],
                 $package
             );
         }
